117534329517ebadbad81024c0a8e9d5f30e2553
Harshada Sasturkar <65084101+harshada-s@users.noreply.github.com>
2023-09-05 21:45:36 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/AnalysisofOrders_16939606925340/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link";
+const url = "https://public.tableau.com/views/AnalysisofBrazilianECommerceMarket/AnalysisofBrazilianECommerceMarket?:language=en-US&:display_count=n&:origin=viz_share_link";
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
